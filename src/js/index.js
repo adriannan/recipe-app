72bc36ecf729b60248c034a154d233884fef6f8a
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-03-28 19:42:52 +0100

    build the shopping list controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state of the app
@@ -97,6 +98,35 @@ const controlRecipe = async () => {
   window.addEventListener(event, controlRecipe)
 );
 
+/* *
+ * LIST CONTROLLER *
+ * */
+const controlList = () => {
+  // Create a new list if there's none yet
+  if (!state.list) state.list = new List();
+  // Add ingredient to the list and the UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+// Handle delete and update list item events
+elements.shoppingList.addEventListener("click", e => {
+  const id = e.target.closest(".shopping__item").dataset.itemid;
+
+  if (e.target.matches(".shopping__delete, .shopping__delete *")) {
+    // delete item from state
+    state.list.deleteItem(id);
+    // delete item from UI
+    listView.deleteItem(id);
+  } else if (e.target.matches(".shopping__count-value")) {
+    // update
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
   if (e.target.matches(".btn-decrease, .btn-decrease *")) {
@@ -109,7 +139,7 @@ elements.recipe.addEventListener("click", e => {
     // increase button is clicked
     state.recipe.updateServings("inc");
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn-add, .recipe__btn-add *")) {
+    controlList();
   }
 });
-
-window.l = new List();
--- src/js/models/List.js
+++ src/js/models/List.js
@@ -13,10 +13,14 @@ export default class List {
       ingredient
     };
     this.items.push(item);
+    return item;
   }
 
   deleteItem(id) {
     const index = this.items.findIndex(item => item.id === id);
     this.items.splice(index, 1);
+  }
+  updateCount(id, newCount) {
+    this.items.find(el => el.id === id).count = newCount;
   }
 }
